0fd20a112387477735452ee5c86b224c0a3ca029
Alejandro Loo <aloozambrano@gmail.com>
2020-10-01 03:40:23 -0500

    gamestore / consoles uploaded to project

--- js/bootstrap-js/app.js
+++ js/bootstrap-js/app.js
@@ -17,3 +17,30 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// GAMES FILTER
+
+$(document).ready(function () {
+
+  $(".filter-button").click(function () {
+    var value = $(this).attr('data-filter');
+
+    if (value == "all") {
+
+      $('.filter').show('1000');
+    } else {
+
+      $(".filter").not('.' + value).hide('3000');
+      $('.filter').filter('.' + value).show('3000');
+
+    }
+
+
+    if ($(".filter-button").removeClass("active")) {
+      $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
+  });
+
+});
